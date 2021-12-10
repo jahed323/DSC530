38c4d9295ae17c454db39b2d84088dfb546b32a9
jahed323 <89809634+jahed323@users.noreply.github.com>
2021-12-10 10:37:02 -0500

    Add files via upload

---
+++ Week2/myCodeJRahman.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 13:05:39 2021
+
+@author: jahed
+"""
+# DSC 530
+# Week 2
+# Programming Assignment Week 2
+# Author: Jahedur Rahman
+# 12/08/2021
+
+print('Hello World! I wonder why that is always the default coding text to start with')
+
+add = 1 + 2
+
+subtract = 4 - 3
+
+multiply = 5 * 6
+
+divide = 8 / 2
+
+concatenate = 'Data ' + 'Science'
+
+new_list = ['DSC', 530, 'Data', 'Exploration']
+
+new_list.append('Analysis')
+
+new_tuple = ('Anaconda', 'Spyder', 'Python', 3.9)
+
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
+print(concatenate)
+print(new_list)
+print(new_tuple)
